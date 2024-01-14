7d05308e694331eba37f0e6b37532d6a5ce613cc
Saurabh-Pandey <saurabh.k.pandey2018@gmail.com>
2024-01-15 00:03:26 +0530

    increase item, decease item , remove item  completed

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -16,10 +16,23 @@ const cartSlice=createSlice({
         clearCart:(state)=>{
             state.cartItems=[];
         },
+        removeItem:(state,action)=>{
+             console.log(action.payload)
+            const itemId=action.payload
+            state.cartItems = state.cartItems.filter((item )=> item.id !== itemId)
+        },
+        increase:(state,action)=>{
+            const cartItem=state.cartItems.find((item)=>item.id===action.payload.id)
+            cartItem.amount=cartItem.amount +1
+        },
+        decrease:(state,{payload})=>{
+            const cartItem=state.cartItems.find((item)=>item.id===payload.id)
+            cartItem.amount=cartItem.amount - 1 ;
+        }
     }
 })
 
 // console.log(cartSlice)
-export const {clearCart}=cartSlice.actions
+export const {clearCart ,removeItem , increase,decrease}=cartSlice.actions
 
 export default cartSlice.reducer
